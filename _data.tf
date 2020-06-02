dc52be474abe18ae36f73d4194ad7c57bf49f64d
vishal <vishal.bhogate07@gmail.com>
2020-06-02 12:16:08 +1000

    Billing Role

--- _data.tf
+++ _data.tf
@@ -1,1 +1,9 @@
-data "type" "name" {}
+data aws_iam_policy_document assume_role_billing {
+  statement {
+    actions = ["sts:Assume"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.idp_account_id}:root"]
+    }
+  }
+}
